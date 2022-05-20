83f85ea83a64ff675060fdf65644082255bc7c15
Hwang Jung Min <hwangstar156@gmail.com>
2022-05-20 16:43:20 +0900

    refactor: errorboundary에 필요없는 코드 삭제

--- react-payments/src/component/common/Errorboundary/Errorboundary.jsx
+++ react-payments/src/component/common/Errorboundary/Errorboundary.jsx
@@ -3,11 +3,11 @@ import { Component } from "react";
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false, error: null };
+    this.state = { hasError: false };
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: true, error };
+    return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
